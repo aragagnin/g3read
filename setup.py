a25de1cfa45477117c3f8b59c9191ad8450b9464
Antonio Ragagnin <57585343+aragagnin@users.noreply.github.com>
2021-02-01 18:34:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,12 @@ setup(
     author='Antonio Ragagnin',
     author_email='antonio.ragagnin@inaf.it',
     license='GPLv3+',
-    packages=['g3read','g3matcha'],
+    #packages=['g3read','g3matcha'],
     #package_dir={'g3read': '.'},
     #data_files = [('prova', ['g3read.py','g3matcha.py'])], 
+    exclude_package_data={
+        '': 'g3pp.py'
+    }
     zip_safe=False
 )
 
