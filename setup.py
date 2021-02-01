750ece245bf3a8efb1d4010ba98949c920fadd00
Antonio Ragagnin <57585343+aragagnin@users.noreply.github.com>
2021-02-01 18:08:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='g3readers',
+    name='g3read',
     version='1.0',
     description='Read Gadget2/3 snapshots, large simulations and catalogues',
     url='https://aragagnin.github.io',
     author='Antonio Ragagnin',
     author_email='antonio.ragagnin@inaf.it',
     license='GPLv3+',
-    packages=['g3readers'],
-    package_dir={'g3readers': '.'},
+    packages=['g3read'],
+    package_dir={'g3read': '.'},
     zip_safe=False
 )
 
